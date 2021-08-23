6505e9d78d3d1367fe02ded5030dbb372033cb61
Édipo Nascimento <esnedipo@gmail.com>
2021-08-23 17:31:15 -0300

    added comments larning section

---
+++ src/academy.devdojo.maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula01OlaDevDojo {
+  public static void main(String[] args) {
+    // This comment won't be displayed..
+    System.out.println("Using comments in Java..");
+    
+    /**
+     * This is a Java doc comment. It won't be displayed also.
+    */
+  }  
+}
