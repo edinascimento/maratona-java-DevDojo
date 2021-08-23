d275ad1c2c2e3aff7dec4b6dd1a485cb196f84fa
Édipo Nascimento <esnedipo@gmail.com>
2021-08-23 17:06:38 -0300

    added some package to structure the project

---
+++ src/academy.devdojo.maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class OlaDevDojo {
+  public static void main(String[] args) {
+    System.out.println("KA_ME_HA_ME-HAaaaaaaaaa!");
+  }
+}
